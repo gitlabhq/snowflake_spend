9ef8183140bae8c39a440d9c7eb47f3bc7dcab75
Taylor A Murphy, PhD <tmurphy@gitlab.com>
2020-03-25 13:22:47 +0000

    Apply suggestion to models/base/snowflake_query_history.sql

--- models/base/snowflake_query_history.sql
+++ models/base/snowflake_query_history.sql
@@ -36,7 +36,7 @@ WITH source AS (
     end_time                          AS query_end_time,
     start_time                        AS query_start_time,
     bytes_spilled_to_local_storage    AS query_bytes_spillover_local,
-    bytes_spilled_to_remote_storage   AS query_spillover_remote,
+    bytes_spilled_to_remote_storage   AS query_bytes_spillover_remote,
     bytes_scanned                     AS query_bytes_scanned
 
   FROM source
